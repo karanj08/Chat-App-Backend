c5feae38dca018e7310619d15b64bc93737c095e
karan <karanjaink8899@gmail.com>
2024-06-03 22:04:01 +0530

    add URL

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:3000",
+  origin: "https://chat-app-snowy-theta.vercel.app/",
   credentials: true,
 };
 app.use(cors(corsOption));
